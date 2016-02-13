d1b2118034e995f8b9ce5bd963d03081cd740082
haden9 <haden9@gmail.com>
2016-02-13 10:12:17 -0600

    Complete uri_spec rq04

--- app/controllers/api/races_controller.rb
+++ app/controllers/api/races_controller.rb
@@ -27,5 +27,12 @@ module Api
       else
       end
     end
+
+    def create
+      if !request.accept || request.accept == '*/*'
+        render plain: :nothing, status: :ok
+      else
+      end
+    end
   end
 end
